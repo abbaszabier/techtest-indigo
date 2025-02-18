9f7f8413effbd3b4100773e6a4d910020e59819b
Abbas Zabier Mohammad <abbas.zabier06@gmail.com>
2025-02-18 12:05:30 +0700

    add react-query for data fetching

---
+++ src/api/provider/index.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+const QueryClientProviderWrapper: React.FC<{ children: React.ReactNode }> = ({
+  children,
+}) => {
+  const queryClient = new QueryClient();
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      {children} <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
+};
+
+export default QueryClientProviderWrapper;
